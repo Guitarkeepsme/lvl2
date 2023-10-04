a59e9e781280e8e252c7192cf5e04ae58fc11e31
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-10-04 11:50:35 +0300

    task 2 thoughts

--- develop/task2/task2.go
+++ develop/task2/task2.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+/* "a4bc2d5e" => "aaaabccddddde"
+"abcd" => "abcd"
+"45" => "" (некорректная строка)
+"" => ""
+*/
+
 func main() {
+	str := "a4bc2d5e"
+
+	runes := []rune(str)
+
+	res := strings.Builder{}
+
+	for i := 0; i < len(str); i++ {
+		// fmt.Println(string(str[i]))
+		if unicode.IsDigit(runes[i]) {
+			fmt.Println(string(runes[i]))
+			res.WriteRune(runes[i])
+		}
+	}
+	fmt.Println(res)
 
 }
