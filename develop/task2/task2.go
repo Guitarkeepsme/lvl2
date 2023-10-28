f1bafede4a372567090693e41f906d569cf0833d
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-10-28 18:02:27 +0300

    task2 comments

--- develop/task2/task2.go
+++ develop/task2/task2.go
@@ -7,12 +7,16 @@ import (
 	"strings"
 )
 
+// Создаём интерфейс распаковки
 type Unpacker interface {
+	// Этот интерфейс включает себя только один метод
 	Unpack()
 }
 
+// Также нам понадобится структура распакованной строки
 type PackedString string
 
+// Главная функция принимает строку пользователя
 func main() {
 	var pkdString PackedString
 
@@ -21,8 +25,10 @@ func main() {
 	_, err := fmt.Scanf("%s", &pkdString)
 	if err != nil {
 		log.Println(err)
+		// Если строка некорректна, выбрасываем панику
+		// согласно условиям задачи
 	} else if pkdString.Unpack() == "" {
-		fmt.Println("Некорректная строка!")
+		panic("Некорректная строка!")
 	} else {
 		fmt.Println("Распакованная строка: ", pkdString.Unpack())
 	}
@@ -31,6 +37,12 @@ func main() {
 // "a4bc2d5e" => "aaaabccddddde"
 
 func (s PackedString) Unpack() string {
+	// Инициализируем несколько переменных:
+	// базовый билдер строки,
+	// ещё один для обработки тех случаев, когда n > 9
+	// временную строку
+	// строку, в которую будет записано число n,
+	// и флаг, фиксирующий экранирование
 	var (
 		sb      strings.Builder
 		sbA     strings.Builder
@@ -38,35 +50,53 @@ func (s PackedString) Unpack() string {
 		amount  string
 		escaped bool
 	)
+	// Строка "amount" необходима для n > 9: сначала мы объединим два числа в строковом выражении
+	// (например, 1+2, составив 12), а затем переведём их в числовой тип
 	for i, v := range s {
+		// Делаем проверку на экранирование: если оно есть,
+		// записываем текущий символ во временную строку
+		// и переключаем флаг
 		if escaped {
 			temp = string(v)
 			sb.WriteRune(v)
 			escaped = false
 			continue
 		}
+
+		// Если текущий символ -- число, делаем следующее:
 		if _, err := strconv.ParseInt(string(v), 0, 64); err == nil {
+			// записываем это число в переменную количества
 			amount = string(v)
+			// Как только дошли до последнего символа в строке
+			// запоминаем количество символов
 			if i == len(s)-1 {
 				amountN, _ := strconv.ParseInt(string(amount), 0, 64)
+				// и проходим циклом по всем символам,
+				// собирая строку в переменную temp
 				for i := int64(0); i < amountN-1; i++ {
 					sb.WriteString(temp)
 					continue
 				}
 				continue
 			}
+			// Фиксируем количество повторений символа строки
 			sbA.WriteString(amount)
 			continue
+			// И если там что-то записано, переводим строку в число
+			// и циклом, равным этому числу, записываем символ в temp
 		} else if amount != "" {
 			amountN, _ := strconv.ParseInt(string(amount), 0, 64)
 			for i := int64(0); i < amountN-1; i++ {
 				sb.WriteString(temp)
 				continue
 			}
+			// После чего очищаем amount
 			amount = ""
+			// И обнуляем счётчик повторений
 			sbA.Reset()
 		}
 
+		// Если попался символ экранирования, переключаем флаг
 		if string(v) == `\` {
 			escaped = true
 			continue
