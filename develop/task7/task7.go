f628926b9cb34f9ae6592ddac86abddf01e10a7e
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-11-11 11:57:00 +0300

    task 7 thoughts

---
+++ develop/task7/task7.go
@@ -0,0 +1,79 @@
+/* Реализовать функцию, которая будет объединять
+один или более done-каналов в single-канал,
+если один из его составляющих каналов закроется.
+
+Очевидным вариантом решения могло бы стать выражение при использованием select,
+которое бы реализовывало эту связь, однако иногда неизвестно общее число done-каналов,
+с которыми вы работаете в рантайме.
+В этом случае удобнее использовать вызов единственной функции,
+которая, приняв на вход один или более or-каналов, реализовывала бы весь функционал.
+
+Определение функции:
+var or func(channels ...<- chan interface{}) <- chan interface{}
+Пример использования функции:
+sig := func(after time.Duration) <- chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		defer close(c)
+		time.Sleep(after)
+}()
+return c
+}
+
+start := time.Now()
+<-or (
+	sig(2*time.Hour),
+	sig(5*time.Minute),
+	sig(1*time.Second),
+	sig(1*time.Hour),
+	sig(1*time.Minute),
+)
+
+fmt.Printf(“fone after %v”, time.Since(start))
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	all := make(chan interface{}) // Этот канал будет сообщать о закрытии какого-либо другого канала
+
+	for i, channel := range channels { // Слушаем каналы
+		go func(ch <-chan interface{}, closer chan interface{}) {
+			select {
+			case <-ch:
+				fmt.Printf("Канал %d закрылся!\n", i)
+				close(all)
+			case <-closer:
+
+			}
+
+		}(channel, all)
+	}
+	return all
+}
+
+func sig(after time.Duration) <-chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		defer close(c)
+		time.Sleep(after)
+	}()
+	return c
+}
+
+func main() {
+	start := time.Now()
+
+	<-or(
+		sig(5*time.Second),
+		sig(3*time.Second),
+		sig(2*time.Second),
+	)
+	fmt.Printf("Время закрытия канала: %v", start)
+}
