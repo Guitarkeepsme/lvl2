3c58c1bc5faeb74b585ceb2e20611fcde7178f90
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-12-13 21:53:11 +0300

    task 6 done

--- develop/task6/task6.go
+++ develop/task6/task6.go
@@ -1,5 +1,4 @@
 /*
-
 Реализовать утилиту -- аналог консольной команды cut (man cut).
 Утилита должна принимать строки через STDIN, разбивать по разделителю (TAB) на колонки и выводить запрошенные.
 
@@ -8,102 +7,53 @@
 -d - "delimiter" - использовать другой разделитель
 -s - "separated" - только строки с разделителем
 */
-
 package main
 
 import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 )
 
-var (
-	fields               int
-	delimiter, separated bool
-)
-
-type applicationParams struct {
-	fields    int
-	delimiter bool
-	separated bool
-}
-
 func main() {
-	var (
-		text string
-		err  error
-	)
-	params := &applicationParams{}
-	setFlags(params)
-
-	fmt.Println(fields)
-	fmt.Println(separated)
-
-	for text != "\n" {
-		text, err = readStdin()
-		if err != nil {
-			log.Fatal(err)
-		}
-		switch {
-		case delimiter:
-		case separated:
-		default:
-			if fields > 0 {
-				fmt.Printf("%s\n", cut(text, fields))
-				os.Exit(1)
-			} else if fields < 0 {
-				fmt.Printf("cut: полей должно быть больше одного, но не %d\n", fields)
-				os.Exit(1)
-			}
-			fmt.Printf("cut: необходим список байтов, символов или полей\n")
-			os.Exit(1)
-
-		}
-	}
-}
-
-// Инициализируем необходимые флаги
-func setFlags(p *applicationParams) {
-	flag.IntVar(&p.fields, "f", 0, "установить номер поля или колонки")
-	flag.BoolVar(&p.delimiter, "d", false, "использовать другой разделитель")
-	flag.BoolVar(&p.separated, "s", false, "выбрать только строки с разделителем")
+	// Определение флагов командной строки
+	fields := flag.Int("f", 0, "выбрать номер поля (колонки)")
+	delimiter := flag.String("d", "\t", "использовать другой разделитель")
+	onlySeparated := flag.Bool("s", false, "только строки с разделителем")
 	flag.Parse()
-}
 
-// Функция для чтения из стдин
-func readStdin() (string, error) {
-	// текст пользователя собирается билдером
-	var text strings.Builder
-	// ридер читает данные
-	reader := bufio.NewReader(os.Stdin)
-	tempText, err := reader.ReadString('\n')
-	if err != nil {
-		return "", err
-	}
-	text.WriteString(tempText)
+	// Чтение строк из STDIN
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
 
-	return text.String(), nil
-
-}
-
-// Функция для дробления текста на фрагменты за счёт разделителя
-func cut(text string, fields int) []string {
-	// Делим текст по табуляциям
-	strs := strings.Split(text, "\t")
-	// Результатом будет слайс строк
-	res := make([]string, 1)
-	// Если отрезков больше одного,
-	// то делаем проверку: в случае если отрезок
-	// содержит \n, к результату аппендим
-	if len(strs) > 1 {
-		for _, v := range strs {
-			if strings.Contains(v, "\n") {
-				res = append(res, strs[fields])
+		// Проверка, содержит ли строка разделитель
+		if !strings.Contains(line, *delimiter) {
+			if *onlySeparated {
+				continue
 			}
+			fmt.Println(line)
+			continue
+		}
+
+		// Разбивка строки на поля
+		fieldsSlice := strings.Split(line, *delimiter)
+
+		// Выбор и вывод запрошенного поля
+		if *fields == 0 {
+			fmt.Println(line)
+		} else if *fields > 0 && *fields <= len(fieldsSlice) {
+			fmt.Println(fieldsSlice[*fields-1])
+		} else {
+			fmt.Fprintf(os.Stderr, "Ошибка: поля под номером %d нет в строке\n", *fields)
+			os.Exit(1)
 		}
 	}
-	return res
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Ошибка при чтении STDIN:", err)
+		os.Exit(1)
+	}
 }
